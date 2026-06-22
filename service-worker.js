62a4ac75b4bb28cdf17c4c242b393b1a1042b789
Kirk Blackinton <68512181+KirkBlackinton@users.noreply.github.com>
2026-06-22 11:38:53 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,22 @@
-const CACHE="flip-spell-v6",ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./cyclops-heads.png","./cyclops-tails.png"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request))));
+const CACHE="flip-spell-v7";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./cyclops-heads-v2.png","./cyclops-tails-v2.png"];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
+});
+
+self.addEventListener("fetch",event=>{
+  if(event.request.mode==="navigate"){
+    event.respondWith(fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put("./index.html",copy));
+      return response;
+    }).catch(()=>caches.match("./index.html")));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
+});
